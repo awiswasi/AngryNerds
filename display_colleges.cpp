013b114bab7dd816ada08e51080d0090d3eccb28
Awf Wiswasi <55606355+awiswasi@users.noreply.github.com>
2021-02-21 01:47:14 -0800

    Update display_colleges.cpp
    to display the colleges in a table from the SQLite database

--- display_colleges.cpp
+++ display_colleges.cpp
@@ -1,14 +1,35 @@
 #include "display_colleges.h"
 #include "ui_display_colleges.h"
+#include <QSqlQuery>
+#include <QSqlQueryModel>
 
 display_colleges::display_colleges(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::display_colleges)
 {
     ui->setupUi(this);
+
+    QSqlDatabase mydb = QSqlDatabase::addDatabase("QSQLITE");
+    mydb.setDatabaseName("C:/sqlite3/displayCampuses.db");
+
+    mydb.open();
 }
 
 display_colleges::~display_colleges()
 {
     delete ui;
+}
+
+void display_colleges::on_pushButton_clicked()
+{
+    display_colleges obj;
+    QSqlQueryModel * model=new QSqlQueryModel();
+
+    QSqlQuery * qry=new QSqlQuery(obj.mydb);
+    //qry->prepare("select MIN(startingColleges) AS startingColleges, dist from initCampuses group by startingColleges");
+    qry->prepare("select campusName, distance from displayCampuses");
+
+    qry->exec();
+    model->setQuery(*qry);
+    ui->tableView->setModel(model);
 }
