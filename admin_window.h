49fe122185583a871ecb0f7e99ad030f6fb124f3
Awf Wiswasi <55606355+awiswasi@users.noreply.github.com>
2021-02-28 12:39:42 -0800

    newest changes

--- admin_window.h
+++ admin_window.h
@@ -2,6 +2,7 @@
 #define ADMIN_WINDOW_H
 
 #include <QDialog>
+#include "edit_universities.h"
 
 namespace Ui {
 class admin_window;
@@ -15,8 +16,12 @@ public:
     explicit admin_window(QWidget *parent = nullptr);
     ~admin_window();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::admin_window *ui;
+    edit_universities *editUni;
 };
 
 #endif // ADMIN_WINDOW_H
