e39df057c02ef82f6d322f9f4f32227cf51ab484
Awf Wiswasi <55606355+awiswasi@users.noreply.github.com>
2021-02-28 12:40:53 -0800

    Update admin_window.cpp

--- admin_window.cpp
+++ admin_window.cpp
@@ -1,5 +1,6 @@
 #include "admin_window.h"
 #include "ui_admin_window.h"
+#include "edit_universities.h"
 
 admin_window::admin_window(QWidget *parent) :
     QDialog(parent),
@@ -11,4 +12,10 @@ admin_window::admin_window(QWidget *parent) :
 admin_window::~admin_window()
 {
     delete ui;
+}
+
+void admin_window::on_pushButton_clicked()
+{
+    editUni = new edit_universities(this);
+    editUni->show();
 }
