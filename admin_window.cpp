70a1acc9486ed08cf29963ade42f13d6b3911d35
Awf Wiswasi <55606355+awiswasi@users.noreply.github.com>
2021-03-08 12:39:19 -0800

    Update admin_window.cpp

--- admin_window.cpp
+++ admin_window.cpp
@@ -1,6 +1,7 @@
 #include "admin_window.h"
 #include "ui_admin_window.h"
 #include "edit_universities.h"
+#include "edit_souvenirs.h"
 
 admin_window::admin_window(QWidget *parent) :
     QDialog(parent),
@@ -18,4 +19,10 @@ void admin_window::on_pushButton_clicked()
 {
     editUni = new edit_universities(this);
     editUni->show();
+}
+
+void admin_window::on_pushButton_2_clicked()
+{
+    editSouv = new edit_souvenirs(this);
+    editSouv->show();
 }
