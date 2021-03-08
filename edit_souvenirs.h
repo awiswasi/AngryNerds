0a5dd711f9f990841f4090f4178578e8f619baf0
Awf Wiswasi <55606355+awiswasi@users.noreply.github.com>
2021-03-08 12:37:04 -0800

    Update edit_souvenirs.h

--- edit_souvenirs.h
+++ edit_souvenirs.h
@@ -2,6 +2,8 @@
 #define EDIT_SOUVENIRS_H
 
 #include <QDialog>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
 
 namespace Ui {
 class edit_souvenirs;
@@ -14,6 +16,20 @@ class edit_souvenirs : public QDialog
 public:
     explicit edit_souvenirs(QWidget *parent = nullptr);
     ~edit_souvenirs();
+    QSqlDatabase souvdb;
+
+private slots:
+    void on_pushButton_3_clicked();
+
+    void on_displaySouvenirsButton_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_searchSouvenirButton_clicked();
 
 private:
     Ui::edit_souvenirs *ui;
